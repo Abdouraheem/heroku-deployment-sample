325827229b55999883f6d7acf9870a9c2ed9ff4f
Abdouraheem <46719420+Abdouraheem@users.noreply.github.com>
2021-11-04 00:29:51 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import flask
+from flask import request
+import pandas as pd
+
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+ 
+if __name__ == '__main__':
+   app.run()
